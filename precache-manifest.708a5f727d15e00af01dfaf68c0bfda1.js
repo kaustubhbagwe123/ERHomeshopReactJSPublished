5158c8f3b750db94a2042e899cec48debf5f35a5
kaustubh.bagwe <kaustubh.bagwe@brainvire.com>
2020-05-09 18:53:11 +0530

    React JS Publish code

---
+++ precache-manifest.708a5f727d15e00af01dfaf68c0bfda1.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47b04351270c8509c784ace103c3d48d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd34f6f579c6e939a801",
+    "url": "/static/css/2.6d96853d.chunk.css"
+  },
+  {
+    "revision": "81d32660dec7ce75edbd",
+    "url": "/static/css/main.b19afaec.chunk.css"
+  },
+  {
+    "revision": "bd34f6f579c6e939a801",
+    "url": "/static/js/2.2989be8b.chunk.js"
+  },
+  {
+    "revision": "81d32660dec7ce75edbd",
+    "url": "/static/js/main.2af24a4e.chunk.js"
+  },
+  {
+    "revision": "7d12240e9fece058070c",
+    "url": "/static/js/runtime~main.0a5c9e36.js"
+  },
+  {
+    "revision": "bfdb66a49be2692b66e794481687ea6f",
+    "url": "/static/media/Bata-shoes.bfdb66a4.jpg"
+  },
+  {
+    "revision": "9c2ae76c3e6945bbff2b8d31d8caa64b",
+    "url": "/static/media/Bata2.9c2ae76c.jpg"
+  },
+  {
+    "revision": "f494895f2098834e2b34add51f5ba8b5",
+    "url": "/static/media/appointments.f494895f.svg"
+  },
+  {
+    "revision": "5863900468bf8a47d8fe026f7c2c6d09",
+    "url": "/static/media/bata.58639004.png"
+  },
+  {
+    "revision": "26c0b7af10ce93bcb6cdb59c3102efc5",
+    "url": "/static/media/bell-icon.26c0b7af.svg"
+  },
+  {
+    "revision": "5c0d67085f5a67a5ae35863386bd269e",
+    "url": "/static/media/braille.5c0d6708.svg"
+  },
+  {
+    "revision": "720a6bce3e41ddec561abd5d17d2c57c",
+    "url": "/static/media/broadCast.720a6bce.png"
+  },
+  {
+    "revision": "d0d589e59d9e3a52f721714c4d406634",
+    "url": "/static/media/calendar-black.d0d589e5.svg"
+  },
+  {
+    "revision": "1ac0121df12abd5605d13c8502548f93",
+    "url": "/static/media/camp-Email.1ac0121d.svg"
+  },
+  {
+    "revision": "b3a525a4efa0d85922c8048f8d58e7cf",
+    "url": "/static/media/campaign.b3a525a4.svg"
+  },
+  {
+    "revision": "8c5d92a116edb47b46fcdde55e900755",
+    "url": "/static/media/chat-bubble-blue.8c5d92a1.svg"
+  },
+  {
+    "revision": "e518b7cd81ec6a0106a888a38d789358",
+    "url": "/static/media/chat-bubble-white.e518b7cd.svg"
+  },
+  {
+    "revision": "5ba9c579784fc2097a3edf09d7f16822",
+    "url": "/static/media/chat-count.5ba9c579.svg"
+  },
+  {
+    "revision": "26946af9eb54118f8637cf87e78529ca",
+    "url": "/static/media/chatbatalogo.26946af9.png"
+  },
+  {
+    "revision": "2698c6dabe6ffbd9d62e500c2c9c7fed",
+    "url": "/static/media/chatbot-icon.2698c6da.svg"
+  },
+  {
+    "revision": "ad7e8d265e71a1b29e041b70c7d06388",
+    "url": "/static/media/cong-img.ad7e8d26.jpg"
+  },
+  {
+    "revision": "806e1f09c1bda852471776d0ca778119",
+    "url": "/static/media/congratulation-final.806e1f09.jpg"
+  },
+  {
+    "revision": "12b32ad05b7df37faac0ea94a3a177aa",
+    "url": "/static/media/delete.12b32ad0.svg"
+  },
+  {
+    "revision": "4667d2e6dceb86effcbe1179fde510bb",
+    "url": "/static/media/dummy-face-1.4667d2e6.png"
+  },
+  {
+    "revision": "fc7208c35fc7049e55512256626cd74c",
+    "url": "/static/media/dummy-face-2.fc7208c3.png"
+  },
+  {
+    "revision": "5b17ce0fc30bd43a9d13ab61b720b729",
+    "url": "/static/media/icon9.5b17ce0f.svg"
+  },
+  {
+    "revision": "323c9b4375bc642989c14a38051df111",
+    "url": "/static/media/icons8-info.323c9b43.svg"
+  },
+  {
+    "revision": "a5be3183a6fc9f33fd260dc0b0ad5a9f",
+    "url": "/static/media/loader.a5be3183.gif"
+  },
+  {
+    "revision": "a9620bd6bbb043345eee232d0e8c21be",
+    "url": "/static/media/logo.a9620bd6.jpg"
+  },
+  {
+    "revision": "f54a6c2b5c1672316e47821435716962",
+    "url": "/static/media/mobile-back.f54a6c2b.svg"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "da5716430015dde0092979512bec9dcc",
+    "url": "/static/media/sent-icon.da571643.svg"
+  },
+  {
+    "revision": "df89d223f7a02eb916a43f77a95a78bf",
+    "url": "/static/media/sms1.df89d223.svg"
+  },
+  {
+    "revision": "616473745bb43d0665c6676c3d81ef1a",
+    "url": "/static/media/sms2.61647374.svg"
+  },
+  {
+    "revision": "1acae5b84ac9cdf4928337f4afb1b7a2",
+    "url": "/static/media/task-new-1.1acae5b8.svg"
+  },
+  {
+    "revision": "55f249c31a963fe721b09c5f60125349",
+    "url": "/static/media/tick.55f249c3.png"
+  },
+  {
+    "revision": "7541b24c78a983724a2385fbc35e4bc2",
+    "url": "/static/media/twitter.7541b24c.png"
+  },
+  {
+    "revision": "ed5d460a2a414994cc3002cfae1fdb5c",
+    "url": "/static/media/whatsapp.ed5d460a.svg"
+  },
+  {
+    "revision": "f87d23920692f5778d7012ed8cb19827",
+    "url": "/static/media/word.f87d2392.png"
+  }
+]);
